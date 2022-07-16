88f8c480ee2343fbab4b2eff1c8cdf196a890974
Patrícia Campos <patricia.r.cam@gmail.com>
2022-07-16 13:17:16 -0300

    [Bug] Ajuste controller

--- src/main/java/br/com/dbc/trabalhofinalmodulo2/controller/ClassePersonagemController.java
+++ src/main/java/br/com/dbc/trabalhofinalmodulo2/controller/ClassePersonagemController.java
@@ -9,9 +9,12 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@RestController
+@RequestMapping("/classePersonagem")
 public class ClassePersonagemController {
 
     @Autowired
