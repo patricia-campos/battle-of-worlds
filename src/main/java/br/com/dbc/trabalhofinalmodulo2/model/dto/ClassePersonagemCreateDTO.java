7ad5e61f3a58b01a0143db4e6fd07da55f69946a
Patrícia Campos <patricia.r.cam@gmail.com>
2022-07-17 19:20:17 -0300

    [Bug] Ajustes em validações.

--- src/main/java/br/com/dbc/trabalhofinalmodulo2/model/dto/ClassePersonagemCreateDTO.java
+++ src/main/java/br/com/dbc/trabalhofinalmodulo2/model/dto/ClassePersonagemCreateDTO.java
@@ -4,10 +4,7 @@ import br.com.dbc.trabalhofinalmodulo2.model.entities.TipoClassePersonagem;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-
 
 @Data
 public class ClassePersonagemCreateDTO {
@@ -16,12 +13,17 @@ public class ClassePersonagemCreateDTO {
     @Schema(description = "Nome da classe do personagem")
     @NotNull(message = "Nome da classe do personagem cenário não pode ser nulo")
     private TipoClassePersonagem tipoClassePersonagem;
-     @NotNull
-    private Double vidaClasse;
-    @NotNull
-    private Double defesaClasse;
-    @NotNull
-    private Double ataqueClasse;
 
+    @Schema(description = "Vida do personagem")
+    @NotNull(message = "Vida do personagem não pode ser nula")
+    private Double vidaClasse;
+
+    @Schema(description = "Defesa do personagem")
+    @NotNull(message = "Defesa do personagem não pode ser nula")
+    private Double defesaClasse;
+
+    @Schema(description = "Ataque do personagem")
+    @NotNull(message = "Ataque do personagem não pode ser nula")
+    private Double ataqueClasse;
 
 }
