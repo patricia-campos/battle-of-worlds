e66857d83b207ce1021b2521d20567f67d3ac574
Patrícia Campos <patricia.r.cam@gmail.com>
2022-07-17 13:55:49 -0300

    [Bug] Ajustes TipoReino

--- src/main/java/br/com/dbc/trabalhofinalmodulo2/model/entities/TipoReino.java
+++ src/main/java/br/com/dbc/trabalhofinalmodulo2/model/entities/TipoReino.java
@@ -1,23 +1,35 @@
 package br.com.dbc.trabalhofinalmodulo2.model.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.util.Arrays;
 
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public enum TipoReino {
 
 
-    LUZ("1"),
-    SOMBRAS("2"),
-    LUA("3"),
-    MARTE("4");
+    LUZ("LUZ", 1),
+    SOMBRAS("SOMBRAS", 2),
+    LUA("LUA", 3),
+    MARTE("MARTE", 4);
 
     private String tipo;
+    private int id;
 
-    TipoReino(String tipo) {
-        this.tipo = tipo;
-    }
 
     public String getTipo() {
         return tipo;
+    }
+
+    public static TipoReino ofTipo(Integer tipo){
+        return Arrays.stream(TipoReino.values())
+                .filter(tp -> tp.getTipo().equals(tipo))
+                .findFirst()
+                .get();
     }
 
     public static TipoReino ofTipo(String tipo){
